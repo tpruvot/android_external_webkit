android/platform_external_webkit

Update WebKit with necessary changes for the Skia merge.
This CL address the modifications needed for r3312 of Skia

Change-Id: I0574657e7a84e53c6b92cefbe07668e9467880db

Source/WebCore/platform/graphics/android/BaseLayerAndroid.h
@@ -26,6 +26,8 @@
 #ifndef BaseLayerAndroid_h
 #define BaseLayerAndroid_h
 
+#include <utils/Mutex.h>
+
 #include "Color.h"
 #include "GLWebViewState.h"
 #include "IntRect.h"

Source/WebCore/platform/graphics/android/ImagesManager.h
@@ -32,6 +32,8 @@
 #include "SkRefCnt.h"
 #include "Vector.h"
 
+#include <utils/Mutex.h>
+
 namespace WebCore {
 
 class ImageTexture;

tpruvot/android_external_webkit

Source/WebCore/platform/graphics/android/BaseLayerAndroid.h
@@ -26,6 +26,8 @@
 #ifndef BaseLayerAndroid_h
 #define BaseLayerAndroid_h
 
+#include <utils/threads.h>
+
 #include "Color.h"
 #include "GLWebViewState.h"
 #include "IntRect.h"

Source/WebCore/platform/graphics/android/ImagesManager.h
@@ -32,6 +32,8 @@
 #include "SkRefCnt.h"
 #include "Vector.h"
 
+#include <utils/threads.h>
+
 namespace WebCore {
 
 class ImageTexture;